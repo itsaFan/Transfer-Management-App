7402dbf02ee2efeec1e7d05d463c099556667e43
itsaFan <steffansim@gmail.com>
2023-08-23 18:43:25 +0800

    add login controller & route

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT,
   mongoUri: process.env.MONGODB_URI,
+  tokenSecret: process.env.JWT_SECRET,
 };
 
 module.exports = config;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
 const userDao = require("../dao/userDao");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
 
 const registerUser = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -19,4 +22,33 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      return res.status(400).json({message: "Email or password is required"})
+    }
+
+    const user = await userDao.findUserByUsername(username);
+    if (!user) { 
+      return res.status(401).json({ message: "Username not found :("})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({message: "Invalid Password :("});
+    }
+    const accessToken = jwt.sign({
+      userId: user._id, 
+      username: user.username,
+      role: user.role,
+    },  
+    config.tokenSecret);
+    return res.status(200).json({ accessToken})
+  } catch(error) {
+    console.error('Internal server error:', error);
+    return res.status(500).json({message: "Internal Server error"})
+  }
+};
+
+module.exports = { registerUser, login };
