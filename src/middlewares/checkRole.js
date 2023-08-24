4ebea8e15405d51d26c1e4eea2d4590aa60bf553
itsaFan <steffansim@gmail.com>
2023-08-24 19:20:37 +0800

    add authorization function

---
+++ src/middlewares/checkRole.js
@@ -0,0 +1,15 @@
+
+
+
+const checkRole = (allowedRoles) => {
+  return (req, res, next) => {
+    const { role } = req.userInfo;
+
+    if (!allowedRoles.includes(role)) {
+      return res.status(403).json({ message: "Access Denied, you don't have permission to access this data" });
+    }
+    next();
+  };
+};
+
+module.exports = {checkRole}
