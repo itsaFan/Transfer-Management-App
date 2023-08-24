e10dc31cb8cd99e0e696c8ef0e8445aff862de88
itsaFan <steffansim@gmail.com>
2023-08-24 16:27:13 +0800

    add transferDao for CRUD op

---
+++ src/dao/transferDao.js
@@ -0,0 +1,31 @@
+const TransferRequest = require("../models/transfer");
+
+const createTransferRequest = async (transferReqData) => {
+  const newTransferRequest = new TransferRequest(transferReqData);
+  await newTransferRequest.save();
+  return newTransferRequest;
+};
+
+const getTransferRequests = async () => {
+  return TransferRequest.find();
+};
+
+const updateTransReqStatus = async (transferId, newStatus) => {
+  try {
+    const filter = { _id: transferId };
+    const update = { status: newStatus };
+
+    const result = await TransferRequest.updateOne(filter, update);
+
+    if (result.nModified === 0) {
+      return null;
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Error when trying to update transfer status:", error);
+    throw error;
+  }
+};
+
+module.exports = { createTransferRequest, getTransferRequests, updateTransReqStatus };
