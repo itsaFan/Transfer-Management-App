ac7024f5a8ee654aab927c227e1aea18b1d6e5a2
itsaFan <steffansim@gmail.com>
2023-08-26 02:43:47 +0800

    add new route for admin

--- src/routes/transferRoutes.js
+++ src/routes/transferRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTransfer, approveTransferReq, rejectTransferReq, getAllTransfers } = require("../controllers/transferController");
+const { createTransfer, approveTransferReq, rejectTransferReq, getAllTransfers, softDeleteTransferReq, adminGetAllTransfers } = require("../controllers/transferController");
 const { verifyJWT } = require("../middlewares/verify");
 const { checkRole } = require("../middlewares/checkRole");
 const { body } = require("express-validator");
@@ -7,8 +7,12 @@ const { body } = require("express-validator");
 const router = express.Router();
 
 router.post("/add", body("amount").trim(), verifyJWT, createTransfer);
-router.put("/approve/:transferId", verifyJWT, checkRole(["approver"]), approveTransferReq);
+router.put("/approve/:transferId", verifyJWT, checkRole(["approver", "admin"]), approveTransferReq);
 router.put("/reject/:transferId", verifyJWT, checkRole(["approver"]), rejectTransferReq);
 router.get("/", getAllTransfers);
+
+//admin area
+router.put("/soft-delete/:transferId", verifyJWT, checkRole(["admin"]), softDeleteTransferReq);
+router.get("/admin/transfer-lists", verifyJWT, checkRole(["admin"]), adminGetAllTransfers)
 
 module.exports = router;
