a98b7664a599447191bbdc22b96bf01e5a605dff
itsaFan <steffansim@gmail.com>
2023-08-25 19:44:44 +0800

    add open-api validation

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const config = require("./src/config/config");
 const dbConnection = require("./src/db/db-connection");
-const authRoutes = require('./src/routes/authRoutes');
-const transferRoutes = require('./src/routes/transferRoutes');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
+const authRoutes = require("./src/routes/authRoutes");
+const transferRoutes = require("./src/routes/transferRoutes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const OpenApiValidator = require("express-openapi-validator");
+
 
 const app = express();
 app.use(express.json());
 
 dbConnection();
 
-
-app.use('/api', authRoutes)
-app.use('/api/transfer', transferRoutes)
-
-//Server documentation 
+//Server documentation
 const swaggerDocument = YAML.load("./src/doc/openapi.yaml");
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use((req, res, next) => {
+  console.log("Open Api Validation Working.");
+  next();
+});
+//OpenApi-Validation Middleware
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: "./src/doc/openapi.yaml",
+    validateRequests: true,
+    //   validateResponses: true,
+  })
+);
 
+app.use("/api", authRoutes);
+app.use("/api/transfer", transferRoutes);
+
+
+//Open-api error handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
 
 app.listen(config.port, () => 
     console.log(`Server is running on port ${config.port}`)
