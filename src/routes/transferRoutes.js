3d8de3baa5039699954bb912368afda7c8b9831c
itsaFan <steffansim@gmail.com>
2023-08-26 05:01:00 +0800

    define route for search by date and status

--- src/routes/transferRoutes.js
+++ src/routes/transferRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTransfer, approveTransferReq, rejectTransferReq, getAllTransfers, softDeleteTransferReq, adminGetAllTransfers } = require("../controllers/transferController");
+const { createTransfer, approveTransferReq, rejectTransferReq, getAllTransfers, softDeleteTransferReq, adminGetAllTransfers, getTransferReqByDateRange, getTransferReqByStatuses } = require("../controllers/transferController");
 const { verifyJWT } = require("../middlewares/verify");
 const { checkRole } = require("../middlewares/checkRole");
 const { body } = require("express-validator");
@@ -13,6 +13,8 @@ router.get("/", getAllTransfers);
 
 //admin area
 router.put("/soft-delete/:transferId", verifyJWT, checkRole(["admin"]), softDeleteTransferReq);
-router.get("/admin/transfer-lists", verifyJWT, checkRole(["admin"]), adminGetAllTransfers)
+router.get("/admin/transfer-lists", verifyJWT, checkRole(["admin"]), adminGetAllTransfers);
+router.get("/search-date", verifyJWT, checkRole(["admin"]), getTransferReqByDateRange,);
+router.get("/search-status", verifyJWT, checkRole(["admin"]), getTransferReqByStatuses);
 
 module.exports = router;
