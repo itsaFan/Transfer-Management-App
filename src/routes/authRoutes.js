41ba6d3746e9e77750177fcb0d760e6100324263
itsaFan <steffansim@gmail.com>
2023-08-25 19:44:31 +0800

    add data sanitation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { registerUser, login } = require("../controllers/authController");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", login)
+router.post("/login", body("username").trim(), login);
 
 module.exports = router;
--- src/routes/transferRoutes.js
+++ src/routes/transferRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { createTransfer, approveTransferReq, rejectTransferReq, getAllTransfers } = require("../controllers/transferController");
 const { verifyJWT } = require("../middlewares/verify");
 const { checkRole } = require("../middlewares/checkRole");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
-router.post("/add", verifyJWT, createTransfer);
+router.post("/add", body("amount").trim(), verifyJWT, createTransfer);
 router.put("/approve/:transferId", verifyJWT, checkRole(["approver"]), approveTransferReq);
 router.put("/reject/:transferId", verifyJWT, checkRole(["approver"]), rejectTransferReq);
 router.get("/", getAllTransfers);
